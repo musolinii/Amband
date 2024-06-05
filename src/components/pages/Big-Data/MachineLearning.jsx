944d47404ea18b4a28c94bcfefa68e43e2279fc9
musolinii <brianbwana7265@gmail.com>
2024-06-05 13:14:22 +0300

    added images to pages

--- src/components/pages/Big-Data/MachineLearning.jsx
+++ src/components/pages/Big-Data/MachineLearning.jsx
@@ -4,52 +4,48 @@ function MachineLearning() {
 
   return (
     <>
-      <div className="bg-slate-800 w-full h-[170px] grid ">
+      <div className="bg-[url('src/assets/images.jpeg')] w-full h-[170px] grid ">
 
-        <p className='place-self-center'>Machine Learning</p>
+        <p className='place-self-center text-white'>Machine Learning</p>
 
       </div>
 
-      <div className="grid md:grid-cols-2 justify-between">
+      <div className="grid grid-cols-2 w-[1200px] h-full ml-[200px]">
+         
+        <div className="w-[600px] h-[445px] mt-[50px]">
+          <div className="text-sm/[14px] px-[15px] leading-relaxed">
 
-        <div className="text-sm/[14px] px-[15px]">
-          <p>
-            Big data analytics is the use of advanced analytic techniques against very large, diverse data sets that include structured, semi-structured and unstructured data, from different sources and sizes ranging from gigabytes to terabytes.
-          </p><br />
-          <p>
-            Big data analytics is the often complex process of examining large and varied data sets, to uncover information — such as hidden patterns, unknown correlations, market trends and customer preferences — that can help organizations make informed business decisions. Analysis of big data allows analysts, researchers, scientists and business users to make better and faster decisions using data that was previously inaccessible or unusable.
-          </p><br />
-          <p>
-            Businesses can use advanced analytics techniques such as text analytics, machine learning, predictive analytics, data mining, statistics, data management and natural language processing to gain new insights from previously untapped data sources independently or together with existing enterprise data.
-          </p><br />
-          <p>
-          Data analytics can also include exploratory data analysis (to identify patterns and relationships in data) and confirmatory data analysis (applying statistical techniques to find out whether an assumption about a particular data set is true or false.)
-          </p>
-        </div>
-        
-        <div className="bg-slate-800 w-[170px] h-[170px] m-[20px]">
+            <h4 className="text-lg/[28px] text-bold mb-[60px]">WHAT IS MACHINE LEARNING ?</h4>
 
+
+            <p>
+              Machine learning (ML) is the scientific study of algorithms and statistical models that computer systems use to perform a specific task without using explicit instructions, relying on patterns and inference instead. It is seen as a subset of artificial intelligence. Machine learning algorithms build a mathematical model based on sample data, known as “training data”, in order to make predictions or decisions without being explicitly programmed to perform the task.     
+            </p><br />
+            <p>
+              Machine learning can also be described as a data analytics technique that teaches computers to do what comes naturally to humans and animals: learn from experience. Machine learning algorithms use computational methods to “learn” directly from data by relying on a predetermined equation as a model.
+            </p><br />
+          </div>
         </div>
 
-        <div className="bg-slate-800 w-[170px] h-[170px] m-[20px]">
+        <div className="w-full h-[445px] m-[20px]">
+          <img src="src/assets/ml.png" alt="data-analytics" className="w-full h-[400px]" />
+        </div>
+
+        <div className="w-full h-[445px] m-[20px]">
+          <img src="src/assets/ml-2.jpeg" alt="data-analytics" className="w-full h-[400px]" />
 
         </div>
 
-        <div className="text-sm/[14px] px-[15px]">
-          <p>
-            Big data analytics is the use of advanced analytic techniques against very large, diverse data sets that include structured, semi-structured and unstructured data, from different sources and sizes ranging from gigabytes to terabytes.
-          </p><br />
-          <p>
-            Big data analytics is the often complex process of examining large and varied data sets, to uncover information — such as hidden patterns, unknown correlations, market trends and customer preferences — that can help organizations make informed business decisions. Analysis of big data allows analysts, researchers, scientists and business users to make better and faster decisions using data that was previously inaccessible or unusable.
-          </p><br />
-          <p>
-            Businesses can use advanced analytics techniques such as text analytics, machine learning, predictive analytics, data mining, statistics, data management and natural language processing to gain new insights from previously untapped data sources independently or together with existing enterprise data.
-          </p><br />
-          <p>
-          Data analytics can also include exploratory data analysis (to identify patterns and relationships in data) and confirmatory data analysis (applying statistical techniques to find out whether an assumption about a particular data set is true or false.)
-          </p>
+        <div className="w-[600px] h-[445px] mt-[50px] ml-[30px]">
+          <div className="text-sm/[14px] px-[15px] leading-relaxed">
+            <p>
+             Machine learning is a branch of Artificial Intelligence based on the idea that systems can learn from data, identify patterns and make decisions without/ with minimal human intervention.</p><br/>       
+            <p>
+              The basic premise of machine learning is to build algorithms that can receive input data and use statistical analysis to predict an output while updating outputs as new data becomes available.
+            </p><br />
+           
+          </div>
         </div>
-
         
 
       </div>
--- src/components/pages/Big-Data/Visualization.jsx
+++ src/components/pages/Big-Data/Visualization.jsx
@@ -25,25 +25,23 @@ function Visualization() {
           </div>
         </div>
 
-        <div className="bg-slate-300 w-full h-[445px] m-[20px]">
-
+        <div className="w-full h-[445px] m-[20px]">
+          <img src="src/assets/graph-1.png" alt="data-analytics" className="w-full h-[400px]" />
         </div>
 
-        <div className="bg-slate-300 w-full h-[445px] m-[20px]">
+        <div className="w-full h-[445px] m-[20px]">
+          <img src="src/assets/graph.png" alt="data-analytics" className="w-full h-[400px]" />
 
         </div>
 
         <div className="w-[600px] h-[445px] mt-[50px] ml-[30px]">
           <div className="text-sm/[14px] px-[15px] leading-relaxed">
             <p>
-              Data visualization is the presentation of data in a pictorial or graphical format. It enables decision makers to see analytics presented visually, so they can grasp difficult concepts or identify new patterns. With interactive visualization, one can take the concept a step further by using technology to drill down into charts and graphs for more detail, interactively changing what data you see and how it’s processed.
-            </p><br />
+              Effective visualization helps users analyze and reason about data and evidence. It makes complex data more accessible, understandable and usable. Users may have particular analytical tasks, such as making comparisons or understanding causality, and the design principle of the graphic (i.e., showing comparisons or showing causality) follows the task. Tables are generally used where users will look up a specific measurement, while charts of various types are used to show patterns or relationships in the data for one or more variables.            </p><br />
             <p>
-              Basically data visualization describes any effort to help people understand the significance of data by placing it in a visual context. Patterns, trends and correlations that might go undetected in text-based data can be exposed and recognized easier with data visualization software.
-            </p><br />
+              The main data visualization goal is to distill large datasets into visual graphics to allow for easy understanding of complex relationships within the data. It is often used interchangeably with terms such as information graphics, statistical graphics, and information visualization.            </p><br />
             <p>
-              It is also a powerful communication tool when done right, it delivers information with both the weight of respected research, and the cut-through clarity of good design.
-            </p><br />
+              Because of the way the human brain processes information, using charts or graphs to visualize large amounts of complex data is easier than poring over spreadsheets or reports. Data visualization is a quick, easy way to convey concepts in a universal manner – and you can experiment with different scenarios by making slight adjustments.            </p><br />
           </div>
         </div>
         
