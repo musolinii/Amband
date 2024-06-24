70c01f95a7acf875c8c0e34f772d0748058c5aa2
musolinii <brianbwana7265@gmail.com>
2024-06-24 16:24:18 +0300

    routing

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -10,7 +10,6 @@ export default function Root() {
         <>
             <Navbar />
             <Outlet />
-            <Home />
             <FooterSection />
             <Footer/>
         </>
