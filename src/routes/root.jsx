200503db849394e22ae40438ccddeabc528a27a3
musolinii <brianbwana7265@gmail.com>
2024-06-24 16:25:06 +0300

    routing

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -2,7 +2,6 @@ import Navbar from "../components/navigation/topnav";
 import Footer from "../components/navigation/footer";
 import { Outlet } from "react-router-dom";
 import FooterSection from "../components/sections/quick-links/FooterSection";
-import Home from "../components/pages/home-page/Home";
 
 
 export default function Root() {
