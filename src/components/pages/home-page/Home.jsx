4bb60f54a910c0508f7d7d6e8ba28a608f1564a9
musolinii <brianbwana7265@gmail.com>
2024-08-06 07:39:23 +0300

    fixed image fetch error

--- src/components/pages/home-page/Home.jsx
+++ src/components/pages/home-page/Home.jsx
@@ -170,7 +170,7 @@ function Home() {
 
         <div className=" w-full sm:h-[135px] place-self-center box-border ">
           <h4 className='text-[28px] font-bold text-center'>WELCOME TO AMBAND</h4>
-          <p className="text-[14px] h-[50px] box-border text-center  mt-[30px] leading-relaxed">We are an innovative and dynamic information technology company that prides itself by offering bespoke and “out of the box” IT related solutions to our clients.</p>
+          <p className="text-[14px] sm:h-[50px] box-border text-center  mt-[30px] leading-relaxed">We are an innovative and dynamic information technology company that prides itself by offering bespoke and “out of the box” IT related solutions to our clients.</p>
         </div>
       </ div>
 
@@ -178,7 +178,7 @@ function Home() {
 
        <div className="bg-slate-200  sm:h-[511px] ">
 
-         <div className="h-[360px] mt-[80px] ml-[80px]">
+         <div className="sm:h-[360px] mt-[80px] ml-[80px]">
 
            <h4 className="text-[28px] font-bold mb-[50px]">WHO WE ARE </h4>
 
@@ -196,7 +196,7 @@ function Home() {
       </div>
 
       <div className="sm:my-[100px] sm:mx-[220px] h-[85px]">
-       <div className='w-full h-[85px] '>
+       <div className='w-full sm:h-[85px] '>
          <h4 className='text-[28px] font-bold text-center py-[20px]'>OUR WORK</h4>
        </div>
       </div>
@@ -204,17 +204,17 @@ function Home() {
 
      <div className="grid grid-cols-1 sm:grid-cols-2">
        <div className='sm:w-[550px] h-full grid gap-2 text-[14px] leading-relaxed'>
-         <div className='sm:w-[534px] h-[170px] mx-[20px]'>
+         <div className='sm:w-[534px] sm:h-[170px] mx-[20px]'>
            <h5 className='font-bold mb-[20px]'>Our Services</h5>
            <p className='mb-[30px]'>Amband offers the following services to our clients; Software and website development, ICT consultancy, ICT support and maintenance, Monitoring and Evaluation, IS audit and IT governance.</p>
            <Link to="/services" className='text-blue-500'>Read More</Link>
          </div>
-         <div className='sm:w-[534px] h-[170px] mx-[20px]'>
+         <div className='sm:w-[534px] sm:h-[170px] mx-[20px]'>
            <h5 className='font-bold mb-[20px]'>Security Products</h5>
            <p className='mb-[30px]'>Amband offers a variety of security services ranging from Absolute software, stoptrack and CCTV installation and maintenance.</p>
            <Link to="/security-products" className='text-blue-500'>Read More</Link>
          </div>
-         <div className='sm:w-[534px] h-[170px] mx-[20px]'>
+         <div className='sm:w-[534px] sm:h-[170px] mx-[20px]'>
            <h5 className='font-bold mb-[20px]'>BIG DATA</h5>
            <p className='mb-[30px]'><b>Big Data</b> brings together <b>data</b> from many disparate sources and applications.</p>
            <Link to="/big-data" className='text-blue-500'>Read More</Link>
@@ -223,12 +223,12 @@ function Home() {
        </div>
 
        <div className='sm:w-[600px] h-full grid gap-2 text-[14px] leading-relaxed'>
-         <div className='sm:w-[534px] h-[170px] mx-[20px]'>
+         <div className='sm:w-[534px] sm:h-[170px] mx-[20px]'>
            <h5 className='font-bold mb-[20px]'>Solutions</h5>
            <p className='mb-[30px]'>Amband offers the following solutions to our clients; Asset management system, Hospital Management Information Systems, Academic and student management systems.</p>
            <Link to="/solutions" className='text-blue-500'>Read More</Link>
          </div>
-         <div className='sm:w-[534px] h-[250px] mx-[20px] '>
+         <div className='sm:w-[534px] sm:h-[250px] mx-[20px] '>
            <h5 className='font-bold mb-[20px]'>RFID</h5>
              <p>Amband offers the following RFID solutions;</p>
                <ul className='mb-[30px]'>
@@ -242,7 +242,7 @@ function Home() {
          <Link to="/RFID-solutions" className='text-blue-500'>Read More</Link>
 
         </div>
-         <div className='sm:w-[534px] h-[170px] mx-[20px]'>
+         <div className='sm:w-[534px] sm:h-[170px] mx-[20px]'>
          <h5 className='font-bold mb-[20px]'>Research</h5>
           <p className='mb-[30px]'>Professional ICT research Consultunts</p>
           <Link to="/research" className='text-blue-500'>Read More</Link>
@@ -252,20 +252,20 @@ function Home() {
      </div>
 
      <div className="w-full sm:h-[377px] grid" >
-        <div className="self-center sm:w-[1200px] h-[228px] sm:ml-[200px] grid grid-cols-1 sm:grid-cols-2 gap-y-0">
+        <div className="self-center sm:w-[1200px] sm:h-[228px] sm:ml-[200px] grid grid-cols-1 sm:grid-cols-2 gap-y-0">
           <div className="sm:w-[370px] sm:h-[228px]">
           <img  src={mobile} alt={"Phone"}/>
           </div>
           <div className="sm:w-[800px] sm:h-[228px] ml-[20px]">
-            <h4 className='text-lg/[28px] font-bold mb-[30px]'>MOBILE APP DEVELOPMENT</h4>
-            <p className="text-sm/[14px] leading-relaxed mr-[30px]">Leverage our mobile application development teams to develop novel apps, which can help you to scale-up your businesses.</p>
+            <h4 className='text-[28px] font-bold mb-[30px]'>MOBILE APP DEVELOPMENT</h4>
+            <p className="text-[14px] leading-relaxed mr-[30px]">Leverage our mobile application development teams to develop novel apps, which can help you to scale-up your businesses.</p>
           </div>
         </div>
 
      </div>
 
      <div className="w-full sm:h-[304px] bg-blue-900 grid">
-       <div className='sm:w-[1170px] h-[154px] sm:ml-[200px] place-self-center text-center'>
+       <div className='sm:w-[1170px] sm:h-[154px] sm:ml-[200px] place-self-center text-center'>
          <h2 className='text-[34px] text-white mb-[20px]'>Impressed With Our Services  ?</h2>
          <p className='text-[14px] text-white'>Leave us a message or request for a services ?</p>
          <button onClick={handleOpen} className='bg-white text-black px-[20px] py-[10px] mt-[20px] text-sm rounded hover:bg-slate-500 transition'>Contact Us</button>
